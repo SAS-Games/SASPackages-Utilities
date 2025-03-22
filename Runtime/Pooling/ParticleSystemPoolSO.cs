6cc1abd33b535865f8acb1133f3af27d5a9b6db8
Abhi86shek <sonkar.abhishek86@gmail.com>
2025-03-22 15:33:22 +0530

    Particle system pool

---
+++ Runtime/Pooling/ParticleSystemPoolSO.cs
@@ -0,0 +1,12 @@
+﻿using SAS.Pool;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SAS/Pool/ParticleSystem")]
+public class ParticleSystemPoolSO : ComponentPoolSO<ParticleSystem>
+{
+	[SerializeField] private ParticleSystemFactorySO _factory;
+	protected override IFactory<ParticleSystem> Factory
+	{
+		get => _factory;
+	}
+}
---
+++ Runtime/Pooling/ParticleSystemSO.cs
@@ -0,0 +1,13 @@
+﻿using SAS.Pool;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SAS/Pool/Factory/ParticleSystem")]
+public class ParticleSystemFactorySO : FactorySO<ParticleSystem>
+{
+    [SerializeField] private ParticleSystem m_ParticleSystem;
+    public override bool Create(out ParticleSystem item)
+    {
+        item = Instantiate(m_ParticleSystem);
+        return m_ParticleSystem.GetComponent<ParticleSystem>();
+    }
+}
