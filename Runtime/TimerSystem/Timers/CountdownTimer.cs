48c54272768edf974156914bfe199de2ee54835b
Abhi86shek <sonkar.abhishek86@gmail.com>
2025-03-22 21:19:39 +0530

    Timer System

---
+++ Runtime/TimerSystem/Timers/CountdownTimer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace SAS.TimerSystem
+{
+    /// <summary>
+    /// Timer that counts down from a specific value to zero.
+    /// </summary>
+    public class CountdownTimer : Timer
+    {
+        public CountdownTimer(float value) : base(value) { }
+        public CountdownTimer() : base(0) { }
+
+        public override void Tick()
+        {
+            if (IsRunning && CurrentTime > 0)
+            {
+                CurrentTime -= Time.deltaTime;
+            }
+
+            if (IsRunning && CurrentTime <= 0)
+            {
+                Stop();
+            }
+        }
+
+        public void Start(float value)
+        {
+            initialTime = value;
+            Start();
+        }
+
+        public override bool IsFinished => CurrentTime <= 0;
+    }
+}
