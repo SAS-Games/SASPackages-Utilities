99cc4d3cc43ced2274a2ac6d1e199f2750f5cd12
Abhi86shek <sonkar.abhishek86@gmail.com>
2025-03-23 18:17:55 +0530

    Update SpawnPoint.cs

--- Runtime/SceneManagement/Teleport/SpawnPoint.cs
+++ Runtime/SceneManagement/Teleport/SpawnPoint.cs
@@ -2,4 +2,13 @@
 
 public class SpawnPoint : MonoBehaviour {
     public string spawnName;
+    public GameObject SpawnedObject { get; set; }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Vector3 center = transform.position + new Vector3(0f, 0.5f, 0f);
+        Gizmos.DrawWireCube(center, Vector3.one);
+        Gizmos.DrawLine(center, center + transform.forward * 2);
+    }
 }
