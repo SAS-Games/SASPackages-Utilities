5321a0673dfd7e841606f992b979c73657087c76
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2025-05-09 23:53:08 +0530

    added unity event in Ontriggerhandler

--- Runtime/Common/OnTriggerHandler.cs
+++ Runtime/Common/OnTriggerHandler.cs
@@ -1,36 +1,39 @@
+using System;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
+using Object = UnityEngine.Object;
 
 public class OnTriggerHandler : MonoBehaviour
 {
-    [SerializeField] private GameObject m_MessageListener;
-    [SerializeField] private string m_OnEnterMessage;
-    [SerializeField] private string m_OnExitMessage;
+    [Obsolete] [SerializeField] private GameObject m_MessageListener;
+    [Obsolete] [SerializeField] private string m_OnEnterMessage;
+    [Obsolete] [SerializeField] private string m_OnExitMessage;
     [SerializeField] private string[] m_CollisionTags = { "Player" };
+    [SerializeField] private UnityEvent<Object> m_OnTriggerEnterAction;
+    [SerializeField] private UnityEvent<Object> m_OnTriggerExitAction;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (string.IsNullOrEmpty(m_OnEnterMessage))
-            return;
-
         if (m_CollisionTags.Contains(other.tag))
         {
             if (!m_MessageListener)
                 m_MessageListener = gameObject;
-            m_MessageListener?.SendMessage(m_OnEnterMessage, other, SendMessageOptions.DontRequireReceiver);
+            if (!string.IsNullOrEmpty(m_OnEnterMessage))
+                m_MessageListener?.SendMessage(m_OnEnterMessage, other, SendMessageOptions.DontRequireReceiver);
+            m_OnTriggerEnterAction?.Invoke(other);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (string.IsNullOrEmpty(m_OnExitMessage))
-            return;
-
         if (m_CollisionTags.Contains(other.tag))
         {
             if (!m_MessageListener)
                 m_MessageListener = gameObject;
-            m_MessageListener?.SendMessage(m_OnExitMessage, other, SendMessageOptions.DontRequireReceiver);
+            if (!string.IsNullOrEmpty(m_OnExitMessage))
+                m_MessageListener?.SendMessage(m_OnExitMessage, other, SendMessageOptions.DontRequireReceiver);
+            m_OnTriggerExitAction?.Invoke(other);
         }
     }
 }
