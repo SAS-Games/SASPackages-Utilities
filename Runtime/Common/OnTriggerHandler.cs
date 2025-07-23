62df20b2a959a62eb0a1279f5aa27d6b7b335b8f
Abhi86shek <sonkar.abhishek86@gmail.com>
2025-07-23 22:44:49 +0530

    removed obsolated

--- Runtime/Common/OnTriggerHandler.cs
+++ Runtime/Common/OnTriggerHandler.cs
@@ -2,26 +2,18 @@ using System;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
-using Object = UnityEngine.Object;
 
 public class OnTriggerHandler : MonoBehaviour
 {
-    [Obsolete] [SerializeField] private GameObject m_MessageListener;
-    [Obsolete] [SerializeField] private string m_OnEnterMessage;
-    [Obsolete] [SerializeField] private string m_OnExitMessage;
     [SerializeField] private string[] m_CollisionTags = { "Player" };
-    [SerializeField] private UnityEvent<Object> m_OnTriggerEnterAction;
-    [SerializeField] private UnityEvent<Object> m_OnTriggerExitAction;
+    [SerializeField] private UnityEvent<GameObject> m_OnTriggerEnterAction;
+    [SerializeField] private UnityEvent<GameObject> m_OnTriggerExitAction;
 
     private void OnTriggerEnter(Collider other)
     {
         if (m_CollisionTags.Contains(other.tag))
         {
-            if (!m_MessageListener)
-                m_MessageListener = gameObject;
-            if (!string.IsNullOrEmpty(m_OnEnterMessage))
-                m_MessageListener?.SendMessage(m_OnEnterMessage, other, SendMessageOptions.DontRequireReceiver);
-            m_OnTriggerEnterAction?.Invoke(other);
+            m_OnTriggerEnterAction?.Invoke(other.gameObject);
         }
     }
 
@@ -29,11 +21,7 @@ public class OnTriggerHandler : MonoBehaviour
     {
         if (m_CollisionTags.Contains(other.tag))
         {
-            if (!m_MessageListener)
-                m_MessageListener = gameObject;
-            if (!string.IsNullOrEmpty(m_OnExitMessage))
-                m_MessageListener?.SendMessage(m_OnExitMessage, other, SendMessageOptions.DontRequireReceiver);
-            m_OnTriggerExitAction?.Invoke(other);
+            m_OnTriggerExitAction?.Invoke(other.gameObject);
         }
     }
 }
