e3aa9c9cc29d448fd2acdb56bdc6e0f88686d557
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2025-09-08 15:19:21 +0530

    exposed event API

--- Runtime/Common/OnTriggerHandler.cs
+++ Runtime/Common/OnTriggerHandler.cs
@@ -9,6 +9,9 @@ public class OnTriggerHandler : MonoBehaviour
     [SerializeField] private UnityEvent<GameObject> m_OnTriggerEnterAction;
     [SerializeField] private UnityEvent<GameObject> m_OnTriggerExitAction;
 
+    public UnityEvent<GameObject> OnTriggerEnterAction => m_OnTriggerEnterAction;
+    public; UnityEvent<GameObject> OnTriggerExitAction => m_OnTriggerExitAction;
+
     private void OnTriggerEnter(Collider other)
     {
         if (m_CollisionTags.Contains(other.tag))
